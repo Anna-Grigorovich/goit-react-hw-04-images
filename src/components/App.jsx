705db03e3d056ee98faba66498d1653d2c8e1c5a
Anna Grigorovich <115174523+Anna-Grigorovich@users.noreply.github.com>
2023-05-27 12:35:58 +0300

    edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,148 +9,76 @@ import { Modal } from './Modal/Modal';
 import { Loader } from './Loader/Loader';
 import { Button } from './Button/Button';
 
-// export class App extends Component {
-//   state = {
-//     input: '',
-//     query: [],
-//     error: null,
-//     status: 'idle',
-//     showModal: false,
-//     largeImg: '',
-//     page: 1,
-//   };
+export function App() {
+  const [input, setInput] = useState('');
+  const [query, setQuery] = useState([]);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('idle');
+  const [showModal, setShowModal] = useState(false);
+  const [largeImg, setLargeImg] = useState('');
+  const [page, setPage] = useState(1);
 
-//   componentDidUpdate(prevProps, prevState) {
-//     const nextQuery = this.state.input;
-//     const prevQuery = prevState.input;
-//     const nextPage = this.state.page;
-//     const prevPage = prevState.page;
-
-//     if (prevQuery !== nextQuery || prevPage !== nextPage) {
-//       this.setState({ status: 'pending' });
-//       getImages(nextQuery, nextPage)
-//         .then(({ hits }) => {
-//           if (prevQuery === nextQuery) {
-//             this.setState({
-//               query: [...prevState.query, ...hits],
-//               status: 'resolved',
-//             });
-//           } else {
-//             this.setState({
-//               query: [...hits],
-//               status: 'resolved',
-//             });
-//           }
-//         })
-//         .catch(error => this.setState({ error, status: 'rejected' }));
-//     }
-//   }
-
-//   formSubmit = input => {
-//     if (input !== this.state.input) {
-//       this.setState({ input: input.toLowerCase(), page: 1 });
-//     }
-//   };
-//   toggleModal = () => {
-//     this.setState(({ showModal }) => ({ showModal: !showModal }));
-//   };
-//   handleClickImg = e => {
-//     const large = e.currentTarget.dataset.src;
-//     this.setState({ largeImg: large, showModal: true });
-//   };
-//   handleClickLoad = () => {
-//     this.setState(({ page }) => ({
-//       page: page + 1,
-//     }));
-//   };
-
-//   render() {
-//     const { showModal, query, largeImg, status, error } = this.state;
-//     return (
-//       <>
-//         <ToastContainer />
-//         {status === 'rejected' && <h1>{error.message}</h1>}
-//         <Searchbar formSubmit={this.formSubmit} />
-//         <ImageGallery query={query} onClickImg={this.handleClickImg} />
-//         {showModal && (
-//           <Modal onClose={this.toggleModal}>
-//             <img src={largeImg} alt="" />
-//           </Modal>
-//         )}
-//         {status === 'pending' && <Loader />}
-//         {query.length >= 12 && <Button onClick={this.handleClickLoad} />}
-//       </>
-//     );
-//   }
-// }
-
-export function App () {
- const [input, setInput] = useState("");
- const [query, setQuery] = useState([]);
- const [error, setError] = useState(null);
- const [status, setStatus] = useState('idle');
- const [showModal, setShowModal] = useState(false);
- const [largeImg, setLargeImg] = useState("");
- const [page, setPage] = useState(1);
-
-useEffect(()=> {
-if(!input) {
-  return;
-}
-setStatus('pending');
-      getImages(input, page)
-        .then(({ hits, total }) => {
-          if (total === 0){
-             toast.warn('Нет ничего с таким названием');
-          }
-        setQuery( prev => {          
-          return [...prev, ...hits];
-        })
-        setStatus('resolved');}
-        )
-        .catch(error =>{ setError(error); setStatus('rejected')});
-    }, [input, page])
-
- const formSubmit = input => {
-      setInput( state => {
-        if (state !== input){
-          setQuery([])
-          return input.toLowerCase();
-        }
-      })
-      setPage(1)
+  useEffect(() => {
+    if (!input) {
+      return;
     }
- const toggleModal = () => {
-  setShowModal(!showModal);
+    setStatus('pending');
+    getImages(input, page)
+      .then(({ hits, total }) => {
+        if (total === 0) {
+          toast.warn('Нет ничего с таким названием');
+        }
+        setQuery(prev => {
+          return [...prev, ...hits];
+        });
+        setStatus('resolved');
+      })
+      .catch(error => {
+        setError(error);
+        setStatus('rejected');
+      });
+  }, [input, page]);
+
+  const formSubmit = newInput => {
+    if (newInput !== input) {
+      setQuery([]);
+      setInput(newInput.toLowerCase());
+      setPage(1);
+    }
+    // setInput( state => {
+    //   if (state !== input){
+    //     setQuery([])
+    //     return input.toLowerCase();
+    //   }
+    // })
+    // setPage(1)
+  };
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
- const handleClickImg = e => {
- const large = e.currentTarget.dataset.src;
+  const handleClickImg = e => {
+    const large = e.currentTarget.dataset.src;
     setLargeImg(large);
     setShowModal(true);
   };
 
- const handleClickLoad = () => {
-  setPage(state => state +1)
-  };  
-return (
-  <>
-    <ToastContainer />
-    {status === 'rejected' && <h1>{error.message}</h1>}
-    <Searchbar formSubmit={formSubmit} />
-    <ImageGallery query={query} onClickImg={handleClickImg} />
-    {showModal && (
-      <Modal onClose={toggleModal}>
-        <img src={largeImg} alt="" />
-      </Modal>
-    )}
-    {status === 'pending' && <Loader />}
-    {query.length >= 12 && <Button onClick={handleClickLoad} />}
-  </>
-);
-
-
+  const handleClickLoad = () => {
+    setPage(state => state + 1);
   };
-
-
-
+  return (
+    <>
+      <ToastContainer />
+      {status === 'rejected' && <h1>{error.message}</h1>}
+      <Searchbar formSubmit={formSubmit} />
+      <ImageGallery query={query} onClickImg={handleClickImg} />
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <img src={largeImg} alt="" />
+        </Modal>
+      )}
+      {status === 'pending' && <Loader />}
+      {query.length >= 12 && <Button onClick={handleClickLoad} />}
+    </>
+  );
+}
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,40 +2,36 @@ import { useEffect } from 'react';
 import c from './Modal.module.css';
 import PropTypes from 'prop-types';
 
-export const Modal = ({onClose, children}) => {
- useEffect (() => {
-  window.addEventListener('keydown', handleEsc);
-  return () => {
-  window.removeEventListener('keydown', handleEsc);
-  }
- })
-  
- const handleEsc = e => {
+export const Modal = ({ onClose, children }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleEsc);
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+    };
+  });
+
+  const handleEsc = e => {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
- const handleBackdropClick = e => {
+  const handleBackdropClick = e => {
     if (e.currentTarget === e.target) {
       onClose();
     }
   };
 
-    return (
-      <div className={c.overlay} onClick={handleBackdropClick}>
-        <div className={c.modal}>{children}</div>
-      </div>
-    );
-  
-}
+  return (
+    <div className={c.overlay} onClick={handleBackdropClick}>
+      <div className={c.modal}>{children}</div>
+    </div>
+  );
+};
 
 Modal.propTypes = {
   onClose: PropTypes.func.isRequired,
   children: PropTypes.any.isRequired,
 };
-
-
-
 
 // export class Modal extends Component {
 //   componentDidMount() {
